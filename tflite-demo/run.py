0eb0bf84d313f710eee569f4607e8d77c35c99b1
Mediavilla, Lino <linomoises.mediavillaponce@sew-eurodrive.it>
2024-06-04 09:40:24 +0200

    update tflite test code

--- tflite-demo/run.py
+++ tflite-demo/run.py
@@ -13,25 +13,26 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Test the model on random input data.
-input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], input_data)
-
-interpreter.invoke()
-
-# The function `get_tensor()` returns a copy of the tensor data.
-# Use `tensor()` in order to get a pointer to the tensor.
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
 
 
-# def image_classifier(inp):
-#     # prediction = my_model.predict(np.array([inp]))[0].tolist()
-#     # class_names = ["bird", "cat", "deer", "dog"]
-#     # print({ k:v for (k,v) in zip(class_names, prediction)} )
-#     # return { k:v for (k,v) in zip(class_names, prediction)} 
-#     return {"cat": 0.3, "dog":0.98}
+def image_classifier(inp):
+    # prediction = my_model.predict(np.array([inp]))[0].tolist()
 
-# demo = gr.Interface(fn=image_classifier, inputs="image", outputs="label")
-# demo.launch()
+    # Test the model on random input data.
+    input_data = np.array([inp], dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    prediction = interpreter.get_tensor(output_details[0]['index'])
+    print(prediction)
+
+    # class_names = ["bird", "cat", "deer", "dog"]
+    # print({ k:v for (k,v) in zip(class_names, prediction)} )
+    # return { k:v for (k,v) in zip(class_names, prediction)} 
+    return {"cat": 0.3, "dog":0.98}
+
+demo = gr.Interface(fn=image_classifier, inputs="image", outputs="label")
+demo.launch()
