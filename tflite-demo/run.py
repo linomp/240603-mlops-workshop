9d8de566cb6c7700e816d45700b44befc8e8591b
Mediavilla, Lino <linomoises.mediavillaponce@sew-eurodrive.it>
2024-06-04 11:05:12 +0200

    fix run.py

--- tflite-demo/run.py
+++ tflite-demo/run.py
@@ -17,24 +17,20 @@ output_details = interpreter.get_output_details()
 
 interpreter_q = tflite.Interpreter(model_path="./model_q_aware.tflite")
 interpreter_q.allocate_tensors()
-input_details = interpreter_q.get_input_details()
-output_details = interpreter_q.get_output_details()
+input_details_q = interpreter_q.get_input_details()
+output_details_q = interpreter_q.get_output_details()
 
 def image_classifier(inp):
-    # Set input and do inference
+    # Set input, do inference and retrieve output
     input_data = np.array([inp], dtype=np.float32)
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
-
-    # The function `get_tensor()` returns a copy of the tensor data.
-    # Use `tensor()` in order to get a pointer to the tensor.
     prediction = interpreter.get_tensor(output_details[0]['index'])[0].tolist()
 
-    # Dp the same with the quantized model to compare
-    input_data = np.array([inp], dtype=np.float32)
-    interpreter_q.set_tensor(input_details[0]['index'], input_data)
+    # Same thing with the quantized model to compare
+    interpreter_q.set_tensor(input_details_q[0]['index'], input_data)
     interpreter_q.invoke()
-    prediction_q = interpreter_q.get_tensor(output_details[0]['index'])[0].tolist()
+    prediction_q = interpreter_q.get_tensor(output_details_q[0]['index'])[0].tolist()
 
     print("raw model: ", { k:v for (k,v) in zip(class_names, prediction)} )
     print("q_aware model: ", { k:v for (k,v) in zip(class_names, prediction_q)} )
