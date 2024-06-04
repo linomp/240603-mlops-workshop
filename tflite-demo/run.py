ebaed52cd76f65282f49815a7a4ca26ebe2a8a23
Mediavilla, Lino <linomoises.mediavillaponce@sew-eurodrive.it>
2024-06-04 09:50:11 +0200

    update tflite test code

--- tflite-demo/run.py
+++ tflite-demo/run.py
@@ -9,11 +9,11 @@ import tflite_runtime.interpreter as tflite
 interpreter = tflite.Interpreter(model_path="./model.tflite")
 interpreter.allocate_tensors()
 
-def image_classifier(inp):
-    # Get input and output tensors.
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
+def image_classifier(inp):
     # Set input and do inference
     input_data = np.array([inp], dtype=np.float32)
     interpreter.set_tensor(input_details[0]['index'], input_data)
@@ -22,7 +22,6 @@ def image_classifier(inp):
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
     prediction = interpreter.get_tensor(output_details[0]['index'])[0].tolist()
-    print(prediction)
 
     class_names = ["bird", "cat", "deer", "dog"]
     print({ k:v for (k,v) in zip(class_names, prediction)} )
