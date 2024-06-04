103869ddd941d4dfc6961b1f8aa68289fbbb0119
Mediavilla, Lino <linomoises.mediavillaponce@sew-eurodrive.it>
2024-06-04 09:41:39 +0200

    update tflite test code

--- tflite-demo/run.py
+++ tflite-demo/run.py
@@ -26,13 +26,13 @@ def image_classifier(inp):
 
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
-    prediction = interpreter.get_tensor(output_details[0]['index'])
+    prediction = interpreter.get_tensor(output_details[0]['index'])[0].tolist()
     print(prediction)
 
-    # class_names = ["bird", "cat", "deer", "dog"]
-    # print({ k:v for (k,v) in zip(class_names, prediction)} )
-    # return { k:v for (k,v) in zip(class_names, prediction)} 
-    return {"cat": 0.3, "dog":0.98}
+    class_names = ["bird", "cat", "deer", "dog"]
+    print({ k:v for (k,v) in zip(class_names, prediction)} )
+    return { k:v for (k,v) in zip(class_names, prediction)} 
+    # return {"cat": 0.3, "dog":0.98}
 
 demo = gr.Interface(fn=image_classifier, inputs="image", outputs="label")
 demo.launch()
