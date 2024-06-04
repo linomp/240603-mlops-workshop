38b19982ae2111316a05755ca705e1f0c658bd82
Mediavilla, Lino <linomoises.mediavillaponce@sew-eurodrive.it>
2024-06-04 09:30:22 +0200

    add bug solution

--- tflite-demo/convert.py
+++ tflite-demo/convert.py
@@ -9,3 +9,16 @@ tflite_model = converter.convert()
 # Save the model.
 with open('model.tflite', 'wb') as f:
   f.write(tflite_model)
+
+
+"""
+This conversion script was failing due to keras version mismatch between the latest tensorflow version and what the tf lite converter expects.
+
+Solution that worked:  
+
+- downgrade local installation of tensorflow: pip install tensorflow==2.15.0
+- run again exercise 4 jupyter notebook to generate again the model artifact, this time with the older keras version
+- run again this conversion script
+
+- source: https://stackoverflow.com/questions/78254377/keras-h5-model-to-tensorflow-lite-tflite-model-conversion
+"""
